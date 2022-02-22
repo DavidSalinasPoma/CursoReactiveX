70934eaa61555a0d7dff11a186bee3113ccb2a1a
David Salinas Poma <favianita2005@gmail.com>
2022-02-22 11:19:14 -0400

    Mi primer observable

---
+++ src/Observables/01-observable.ts
@@ -0,0 +1,64 @@
+
+import { Observable, Observer } from 'rxjs';
+
+// El observer es una interfas
+// obliga a hacer todo lo que se necesita para que ese observer sea valido
+const observer: Observer<any> = {
+    next: value => console.log('Siguiente [next]', value),
+    error: error => console.warn('error [obs]', error),
+    complete: () => console.info('Completado [obs]')
+
+}
+
+// Es para crear un Observable
+// const obs$ = Observable.create();
+
+// Esta definicion de observables permite crear suscripciones
+// Suscripcion: gente que va estar pendiente de las emisiones de los observables
+const obs$ = new Observable<string>(subs => {
+    // Emitimos valores
+    subs.next('Hola');
+    subs.next('Mundo');
+    subs.next('Hola');
+    subs.next('Mundo');
+
+    // Forzar error
+    // const a = undefined;
+    // a.nombre = 'David';
+
+    // Ninguna emision posterior a a la llamada del complete
+    // va a ser notificada a sus suscriptores
+    subs.complete();
+    // Es decir que estos no van a ser notificados
+    subs.next('Hola');
+    subs.next('Mundo');
+});
+
+
+// Nos suscribimos al observable
+// Formas
+//1.- obs$.subscribe(resp=>console.log());
+//2.- obs$.subscribe(console.log);
+//3.- obs$.subscribe(
+//     valor => console.log('Next: ', valor),
+//     error => console.warn('error', error),
+//     () => console.info('Completado')
+// );
+
+//4.- obs$.subscribe(
+//     resp => {
+//         console.log('Next', resp);
+//     },
+//     (err) => {
+//         console.log('Error', err);
+//     },
+//     () => {
+//         console.log('Completado');
+//     }
+// )
+
+//5.- Quinta forma definitiva con el Observer
+obs$.subscribe(observer);
+
+
+
---
+++ src/index.ts
@@ -0,0 +1,48 @@
+
+import { Observable, Observer, Subscriber } from 'rxjs';
+
+// El observer es una interfas
+// obliga a hacer todo lo que se necesita para que ese observer sea valido
+const observer: Observer<any> = {
+    next: value => console.log('next', value),
+    error: error => console.warn('error', error),
+    complete: () => console.info('Completado')
+
+}
+
+const intervalo$ = new Observable<number>(subscriber => {
+    // Crear un contador 1,2,3,4,5,.....
+    let cont: number = 0;
+    const interval = setInterval(() => {
+        // Cada segundo
+        subscriber.next(cont);
+        cont++;
+        console.log(cont);
+
+    }, 1000);
+
+    setTimeout(() => {
+        subscriber.complete();
+    }, 2500);
+
+    return () => {
+        clearInterval(interval);
+        console.log('Intervalo destruido');
+
+    }
+})
+
+// Aqui estamos almacenando la subscripción
+const subs1 = intervalo$.subscribe(observer);
+const subs2 = intervalo$.subscribe(observer);
+const subs3 = intervalo$.subscribe(observer);
+
+// subs1.add(subs2)
+//     .add(subs3);
+
+setTimeout(() => {
+    subs1.unsubscribe();
+    subs2.unsubscribe();
+    subs3.unsubscribe();
+    console.log('Completado timeout');
+}, 6000);
